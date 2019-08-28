2f37008a54379f41200642e9fcfa0654c70fbd99
ctidev09 <ctidev09@gmail.com>
2019-08-28 12:21:35 -0500

    commit428 ago

--- js/script.js
+++ js/script.js
@@ -52,6 +52,11 @@
         adaptiveHeight: true,
     });
 
+    window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden"; // class "loader hidden"
+    });
+
     
 
 
