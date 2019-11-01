404d8023a43c2ca741758085ecdb09a8a8f644e5
ctidev09 <ctidev09@gmail.com>
2019-11-01 13:34:41 -0600

    011119I

--- js/translate/translate.js
+++ js/translate/translate.js
@@ -17,18 +17,32 @@ imports('languajes.js');
 // function change language
 const changeLanguage = () => {
   language_selected = select.value
-
-  elements = document.querySelectorAll('[data-lang]')
-  elements.forEach( element => {
-    current_attr = element.getAttribute('data-lang')
-    element.innerHTML = lenguajes[ language_selected ][current_attr]
-  })
+  localStorage.setItem('select_language', language_selected)
+  console.log("change", language_selected, select.value)
+  setTimeout(() => {
+    console.log("a pintar", language_selected)
+    elements = document.querySelectorAll('[data-lang]')
+    elements.forEach( element => {
+      current_attr = element.getAttribute('data-lang')
+      element.innerHTML = lenguajes[ language_selected ][current_attr]
+    })
+  }, 10);
 }
 
 //get select of languajes
 select = document.getElementById('lang_select')
 
+
 //change language with select change
 select.addEventListener('change', changeLanguage);
 
 
+console.log("carga:", localStorage.getItem('select_language'))
+if(localStorage.getItem('select_language') !== null){
+  console.log("entra a change")
+ // select.value = localStorage.getItem('select_language')
+ $('#lang_select').val(localStorage.getItem('select_language'))
+  changeLanguage();
+  console.log("val:", select.value)
+ /* $('#lang_select').trigger('change')*/
+}
